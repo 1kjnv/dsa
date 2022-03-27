4f9b0cbb09b6a8b9b0b4ec8bbbb463e1f6696f58
abdurauf.isokjonov <abdurauf.isokjonov@slexn.com>
2022-03-27 22:24:33 +0900

    Singly Linked List implementation

---
+++ data_structures/LinkedList/singly.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *next;
+};
+
+struct node* new(int val)
+{
+	struct node* tmp = (struct node*) malloc(sizeof(struct node));
+	tmp->data = val;
+	tmp->next = NULL;
+
+	return tmp;
+}
+
+// insert a new node at the beginning
+// Time complexity is O(1)
+void insert(struct node **head, int val)
+{
+	struct node *newNode = new(val);
+	newNode->next = *head;
+	*head = newNode;
+}
+
+// insert after a given node
+// Time complexity is O(1)
+void insertAfter(struct node *prev, int val)
+{
+	if(prev == NULL)
+	{
+		return;
+	}
+	struct node *newNode = new(val);
+	newNode->next = prev->next;
+	prev->next = newNode;
+}
+
+// insert at the end of the list
+// Time complexity is O(n)
+void append(struct node **head, int val)
+{
+	if(*head == NULL)
+	{
+		*head = new(val);
+		return;
+	}
+	struct node *newNode = new(val);
+	struct node *tmp = *head;
+
+	while(tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+
+	tmp->next = newNode;
+}
+
+// delete a pointer to the head
+// and a key, delete the first occurrence
+// of the key in the list ITERATIVELY
+void delete(struct node **head, int key)
+{
+	struct node *tmp = *head, *prev;
+	if(tmp == NULL)
+	{
+		return;
+	}
+
+	if(tmp != NULL && tmp->data == key)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return;
+	}
+
+	while(tmp != NULL && tmp->data != key)
+	{
+		prev = tmp;
+		tmp = tmp->next;
+	}
+
+	if(tmp == NULL)
+	{
+		return;
+	}
+
+	prev->next = tmp->next;
+	free(tmp);
+}
+
+// delete recursively
+void delete_r(struct node **head, int key)
+{
+	if(*head == NULL)
+	{
+		return;
+	}
+	if((*head)->data == key)
+	{
+		struct node *tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+		return;
+	}
+	delete_r(&(*head)->next, key);
+}
+
+void traverse(struct node *head)
+{
+	while(head != NULL)
+	{
+		printf("%d ", head->data);
+		head = head->next;
+	}
+	printf("\n");
+}
+
+int main()
+{
+	struct node *head = new(23);
+
+	insert(&head, 24);
+	insert(&head, 64);
+	insert(&head, 13);
+
+	insertAfter(head, 112);
+
+	append(&head, 97);
+	delete(&head, 64);
+	delete_r(&head, 13);
+
+	traverse(head);
+
+	free(head);
+
+	return 0;
+}
