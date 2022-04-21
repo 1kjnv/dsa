24aded448f78b85f44cd0f01d1487aefb100766d
ikjnv <abdurauf.isokjonov@gmail.com>
2022-04-21 20:29:58 +0900

    priority queue implementation using array

---
+++ data_structures/priority_queue/priority_queue.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+
+int size = 0;
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void heapify(int arr[], int size, int i)
+{
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+
+	if(left < size && arr[left] > arr[largest])
+	{
+		largest = left;
+	}
+	if(right < size && arr[right] > arr[largest])
+	{
+		largest = right;
+	}
+	if(largest != i)
+	{
+		swap(&arr[i], &arr[largest]);
+		heapify(arr, size, largest);
+	}
+}
+
+void insert(int arr[], int val)
+{
+	if(size == 0)
+	{
+		arr[0] = val;
+		size++;
+	}
+	else
+	{
+		arr[size] = val;
+		++size;
+		for(int i = size / 2 - 1; i >= 0; i--)
+		{
+			heapify(arr, size, i);
+		}
+	}
+}
+
+void delete(int arr[], int num)
+{
+	int i;
+	for(i = 0; i < size; i++)
+	{
+		if(num == arr[i])
+		{
+			break;
+		}
+	}
+
+	swap(&arr[i], &arr[size-1]);
+	--size;
+	for(i = size / 2 - 1; i >= 0; i--)
+	{
+		heapify(arr, size, i);
+	}
+}
+
+void print_array(int arr[], int size)
+{
+	for(int i = 0; i < size; ++i)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	int arr[10];
+
+	insert(arr, 2);
+	insert(arr, 4);
+	insert(arr, 5);
+	insert(arr, 7);
+	insert(arr, 8);
+	insert(arr, 12);
+	
+	printf("Priority queue: ");
+	print_array(arr, size);
+
+	return 0;
+}
